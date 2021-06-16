a6b3a5de1fa30501057262ee7d1831f4b61bd56a
raik.tatyana <raik.tatyana@gmail.com>
2021-06-16 23:11:36 +0300

    selenoid support

--- registrationFormJenkins/src/test/java/tests/TestBase.java
+++ registrationFormJenkins/src/test/java/tests/TestBase.java
@@ -2,11 +2,13 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
   @BeforeAll
   public static void setup() {
     Configuration.startMaximized = true;
+    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
   }
 }
