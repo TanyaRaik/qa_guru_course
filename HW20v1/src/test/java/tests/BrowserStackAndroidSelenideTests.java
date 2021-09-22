13dffaea7e75b47fe101f5a8df650ca46e6055d7
raik.tatyana <raik.tatyana@gmail.com>
2021-09-23 00:22:18 +0300

    upd

--- HW20v1/src/test/java/tests/BrowserStackAndroidSelenideTests.java
+++ HW20v1/src/test/java/tests/BrowserStackAndroidSelenideTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -14,15 +15,39 @@ import static io.qameta.allure.Allure.step;
 @Tag("selenide_android")
 public class BrowserStackAndroidSelenideTests extends TestBase {
 
-    @Test
-    @DisplayName("Successful search in wikipedia android app")
-    void searchTest() {
-        step("Type search", () -> {
-            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
-            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).val("BrowserStack");
-        });
-        step("Verify content found", () ->
-                $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
-                        .shouldHave(sizeGreaterThan(0)));
-    }
+  @Test
+  @DisplayName("Successful search in wikipedia android app")
+  void searchTest() {
+    step("Type search", () -> {
+      $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+      $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).val("BrowserStack");
+    });
+    step("Verify content found", () ->
+            $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
+                    .shouldHave(sizeGreaterThan(0)));
+  }
+
+  @Test
+  @DisplayName("Header wikipedia android app")
+  void checkHeaderTest() {
+    step("header is Displayed", () ->
+            $$(MobileBy.id("org.wikipedia.alpha:id/single_fragment_toolbar_wordmark"))
+                    .shouldBe(CollectionCondition.itemWithText("sizeGreaterThan(0)")));
+  }
+
+  @Test
+  @DisplayName("Options of article in wikipedia android app")
+  void optionsOfArticleTest() {
+    step("Click more options", () -> {
+      $(MobileBy.id("org.wikipedia.alpha:id/view_list_card_header_menu")).click();
+    });
+
+    step("Verify available options", () -> {
+      $$(MobileBy.id("org.wikipedia.alpha:id/title")).shouldHave(sizeGreaterThan(0));
+
+      $$(MobileBy.id("org.wikipedia.alpha:id/single_fragment_toolbar_wordmark"))
+              .shouldBe(CollectionCondition.itemWithText("Hide this card"));
+
+    });
+  }
 }
